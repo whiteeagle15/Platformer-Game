02b144e905da0a560fd148206e68c8dde7cb6fc1
whiteeagle15 <ykap08@gmail.com>
2021-11-28 18:17:17 -0500

    Init Commit

---
+++ src/scenes/Credits.js
@@ -0,0 +1,23 @@
+
+import BaseScene from './BaseScene'
+
+
+class CreditsScene extends BaseScene {
+
+    constructor(config) {
+        super('CreditsScene', {...config, canGoBack: true})
+
+        this.menu = [
+            {scene: null, text: 'Thank you for playing!'},
+            {scene: null, text: 'Author: Yusuf'},
+
+        ]
+    }       
+
+    create() {
+        super.create()
+        this.createMenu(this.menu, () => {})
+    }
+}
+
+export default CreditsScene
